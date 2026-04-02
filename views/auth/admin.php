a82e88049c78621d3f0cbf8cb153035a77977188
M0azem <mohaazem71@gmail.com>
2026-04-02 22:12:04 +0300

    Add files via upload

---
+++ views/auth/admin.php
@@ -0,0 +1,6 @@
+<h2>Admin Login</h2>
+<form method="POST" action="index.php?url=auth/adminLogin">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
---
+++ views/auth/login.php
@@ -0,0 +1,9 @@
+<h2>Login</h2>
+
+<form method="POST" action="index.php?url=auth/check">
+    <input type="email" name="email" placeholder="Email" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
+
+<a href="index.php?url=auth/register">Create Account</a>
